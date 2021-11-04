cc8c4070c04aca3e05b40f4219d2312e1a2d4711
DeskTopDarwin <93140023+DeskTopDarwin@users.noreply.github.com>
2021-11-04 13:32:34 -0400

    added movement

---
+++ Assets/Scripts/InputToV2EventSys.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class InputToV2EventSys : MonoBehaviour
+{
+    public UnityEvent<Vector2> dirPressed;
+
+    public KeyCode up = KeyCode.W;
+    public KeyCode down = KeyCode.S;
+    public KeyCode left = KeyCode.A;
+    public KeyCode right = KeyCode.D;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 toSend = new Vector2();
+        if (Input.GetKey(up))
+            toSend.y = 1;
+        else if (Input.GetKey(down))
+            toSend.y = -1;
+
+        if (Input.GetKey(left))
+            toSend.x = -1;
+        else if (Input.GetKey(right))
+            toSend.x = 1;
+
+        dirPressed.Invoke(toSend);
+    }
+}
---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,68 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public float speed;
+    private Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = gameObject.GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+   //public void movement(KeyCode keyCode)
+   //{
+   //    //UpDown
+   //    if (keyCode == KeyCode.W)
+   //    {
+   //        rb.velocity = new Vector2(0, speed);
+   //    }
+   //    else if (keyCode == KeyCode.S)
+   //    {
+   //        rb.velocity = new Vector2(0, -speed);
+   //    }
+   //    //LeftRigth
+   //    if(keyCode == KeyCode.A)
+   //    {
+   //        rb.velocity = new Vector2(-speed, 0);
+   //    }
+   //    else if(keyCode == KeyCode.D)
+   //    {
+   //        rb.velocity = new Vector2(speed, 0);
+   //    }
+   //    
+   //}
+
+    public void movement(Vector2 direction)
+    {
+        Debug.Log(direction);
+        rb.velocity = new Vector2(0, 0);
+        //UpDown
+        if (direction.y > 0.01f)
+        {
+            rb.velocity = new Vector2(0, direction.y * speed);
+        }
+        else if (direction.y < -0.01f) 
+        {
+            rb.velocity = new Vector2(0, direction.y * speed);
+        }
+        //LeftRight
+        if (direction.x < -0.01f) 
+        {
+            rb.velocity = new Vector2(direction.x *speed, 0);
+        }
+        else if (direction.x > 0.01f) 
+        {
+            rb.velocity = new Vector2(direction.x * speed, 0);
+        }
+    }
+}
